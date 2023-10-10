ae75263608a4d8a7de214885c71c02b6ba821205
Gilberto Mascena <gilbertomascena@gmail.com>
2023-10-09 22:47:21 -0300

    Obtendo um usuário por id

--- src/main/java/com/gilbertodev/springmongo/services/UserService.java
+++ src/main/java/com/gilbertodev/springmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.gilbertodev.springmongo.services;
 
 import com.gilbertodev.springmongo.domain.User;
 import com.gilbertodev.springmongo.repositories.UserRepository;
+import com.gilbertodev.springmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,6 +16,12 @@ public class UserService {
     private UserRepository repo;
 
     public List<User> findAll(){
+
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
