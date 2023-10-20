6cc88e359972a6fe2a3d3e418f31a6055a45b354
Gilberto Mascena <gilbertomascena@gmail.com>
2023-10-20 18:39:02 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/gilbertodev/springmongo/config/Instantiation.java
+++ src/main/java/com/gilbertodev/springmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.gilbertodev.springmongo.config;
 
 import com.gilbertodev.springmongo.domain.Post;
 import com.gilbertodev.springmongo.domain.User;
+import com.gilbertodev.springmongo.dto.AuthorDTO;
 import com.gilbertodev.springmongo.repositories.PostRepository;
 import com.gilbertodev.springmongo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,10 +38,11 @@ public class Instantiation implements CommandLineRunner {
         User Jose = new User(null, "Jose Orange", "jose@gmail.com");
         User Rita = new User(null, "Rita Grey", "rita@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("20/10/2023"), "Partiu viagen", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("19/10/2023"), "Bom dia", "Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob, Carlos, Jose, Rita));
+
+        Post post1 = new Post(null, sdf.parse("20/10/2023"), "Partiu viagen", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("19/10/2023"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
