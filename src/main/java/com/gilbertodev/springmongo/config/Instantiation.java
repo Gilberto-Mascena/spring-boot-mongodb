16ae8c11fc4af4785b54f0eff5f36305d66220d5
Gilberto Mascena <gilbertomascena@gmail.com>
2023-10-20 18:20:36 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/gilbertodev/springmongo/config/Instantiation.java
+++ src/main/java/com/gilbertodev/springmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.gilbertodev.springmongo.config;
 
+import com.gilbertodev.springmongo.domain.Post;
 import com.gilbertodev.springmongo.domain.User;
+import com.gilbertodev.springmongo.repositories.PostRepository;
 import com.gilbertodev.springmongo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -14,10 +18,17 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
-        userRepository.deleteAll();;
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
@@ -26,6 +37,10 @@ public class Instantiation implements CommandLineRunner {
         User Jose = new User(null, "Jose Orange", "jose@gmail.com");
         User Rita = new User(null, "Rita Grey", "rita@gmail.com");
 
+        Post post1 = new Post(null, sdf.parse("20/10/2023"), "Partiu viagen", "Vou viajar para São Paulo. Abraços!", maria);
+        Post post2 = new Post(null, sdf.parse("19/10/2023"), "Bom dia", "Acordei feliz hoje!", maria);
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob, Carlos, Jose, Rita));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
--- src/main/java/com/gilbertodev/springmongo/services/UserService.java
+++ src/main/java/com/gilbertodev/springmongo/services/UserService.java
@@ -6,7 +6,6 @@ import com.gilbertodev.springmongo.repositories.UserRepository;
 import com.gilbertodev.springmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
